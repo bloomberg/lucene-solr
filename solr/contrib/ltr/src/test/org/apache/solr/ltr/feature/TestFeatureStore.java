aa315a5aa557f79ea631a8482914863814621ffc
Christine Poerschke <cpoerschke@apache.org>
2016-08-30 12:19:48 +0100

    TestFeatureStore.testDefaultFeatureStoreName now also tests getFeatureStore(null)

--- solr/contrib/ltr/src/test/org/apache/solr/ltr/feature/TestFeatureStore.java
+++ solr/contrib/ltr/src/test/org/apache/solr/ltr/feature/TestFeatureStore.java
@@ -43,6 +43,9 @@ public class TestFeatureStore extends TestRerankBase {
   public void testDefaultFeatureStoreName()
   {
     assertEquals("_DEFAULT_", FeatureStore.DEFAULT_FEATURE_STORE_NAME);
+    final FeatureStore expectedFeatureStore = fstore.getFeatureStore(FeatureStore.DEFAULT_FEATURE_STORE_NAME);
+    final FeatureStore actualFeatureStore = fstore.getFeatureStore(null);
+    assertEquals("getFeatureStore(null) should return the default feature store", expectedFeatureStore, actualFeatureStore);
   }
 
   @Test
