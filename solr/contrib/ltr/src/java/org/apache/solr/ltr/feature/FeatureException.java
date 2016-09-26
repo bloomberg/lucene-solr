1da60a38f15f1b30b21e9705e6817a78d61a5b27
Christine Poerschke <cpoerschke@apache.org>
2016-09-26 18:53:41 +0100

    Relocate FeatureException from ltr.util to ltr.feature package.

--- solr/contrib/ltr/src/java/org/apache/solr/ltr/feature/FeatureException.java
+++ solr/contrib/ltr/src/java/org/apache/solr/ltr/feature/FeatureException.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.solr.ltr.util;
+package org.apache.solr.ltr.feature;
 
 public class FeatureException extends RuntimeException {
 
